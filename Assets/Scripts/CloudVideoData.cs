03a380ed01be70336de48d3452360e9166704221
Theoness1 <sasha.bolotskih2002@gmail.com>
2024-07-26 23:32:42 +0700

    URL config

---
+++ Assets/Scripts/CloudVideoData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CloudVideoData", menuName = "ScriptableObjects/CloudVideoData", order = 2)]
+public class CloudVideoData : ScriptableObject
+{
+    public string[] ClipsURL;
+}
