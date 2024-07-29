48861ecd17e85eefe06927b850886d4fda39bf1f
Theoness1 <sasha.bolotskih2002@gmail.com>
2024-07-29 20:14:20 +0700

    new ImageLoader
    Loads images via a link and saves the cache in the Assets/Cache/Preview

---
+++ Assets/Scripts/ImageLoader.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ImageLoader : MonoBehaviour
+{
+    [SerializeField] private RawImage[] _rawImages;
+    [SerializeField] private List<string> _imageUrls;
+
+    private void Start()
+    {
+        StartCoroutine(LoadImage());
+    }
+
+    private IEnumerator LoadImage()
+    {
+        for (int i = 0; i < _imageUrls.Count; i++)
+        {
+            string url = _imageUrls[i];
+            string cachePath = Path.Combine(
+                Application.dataPath + "/Cache/Preview", "cachedImage" + i.ToString() + ".png");
+
+            if (File.Exists(cachePath))
+            {
+                byte[] fileData = File.ReadAllBytes(cachePath);
+                Texture2D texture = new Texture2D(2, 2);
+                texture.LoadImage(fileData);
+                _rawImages[i].texture = texture;
+            }
+            else
+            {
+                using UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+                yield return www.SendWebRequest();
+
+                if (www.result == UnityWebRequest.Result.Success)
+                {
+                    Texture2D texture = DownloadHandlerTexture.GetContent(www);
+                    _rawImages[i].texture = texture;
+
+                    byte[] textureBytes = texture.EncodeToPNG();
+                    File.WriteAllBytes(cachePath, textureBytes);
+                }
+                else
+                {
+                    Debug.LogError("Ошибка загрузки изображения: " + www.error);
+                }
+            }
+        }
+    }
+}
